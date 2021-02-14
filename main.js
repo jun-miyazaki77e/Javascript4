e606c260795cfd2a1a518ff24b5f1344177c5886
EC2 Default User <ec2-user@ip-172-31-14-158.ap-northeast-1.compute.internal>
2021-02-14 10:04:55 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+$(function(){
+  $('.slideShow').slick({
+    accessibility: true,
+    autoplay: true,
+    arrrows: true,
+    prevArrow: '<button class="slide-arrow prev-arrow"></button>',
+    nextArrow: '<button class="slide-arrow next-arrow"></button>',
+  });
+});
